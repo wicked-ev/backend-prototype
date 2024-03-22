e383127c93e41738019c86ddd8525e9a33b34383
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2024-03-22 18:51:31 +0100

    added short hand function to store data (not complete)

--- src/DDIG/ddig.service.ts
+++ src/DDIG/ddig.service.ts
@@ -4,6 +4,8 @@ import { DDIGDto } from './dto/DDIG.dto';
 import * as argon from 'argon2';
 import { ConfigService } from '@nestjs/config';
 import { connect, IClientOptions } from 'mqtt/*';
+import { mkdirSync, statSync, writeFile, writeFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 
 @Injectable()
 export class DdigService {
@@ -37,18 +39,23 @@ export class DdigService {
     console.log(Topic);
     return Topic;
   }
-  connection(topic: string, deviceOwnerID: number) {
+
+  CreateMqttOption(DeviceOwnerID: number) {
     const option: IClientOptions = {
       keepalive: 60,
-      clientId: deviceOwnerID.toString(),
+      clientId: DeviceOwnerID.toString(),
       protocolId: 'MQTT',
       protocolVersion: 4,
       clean: true,
       reconnectPeriod: 1000,
       connectTimeout: 30 * 1000,
     };
+    return option;
+  }
+
+  connection(topic: string, deviceOwnerID: number, OwnerID: number) {
     const BrokerUrl = this.config.get('BROKER_URL');
-    const client = connect(BrokerUrl, option);
+    const client = connect(BrokerUrl, this.CreateMqttOption(deviceOwnerID));
     client.on('connect', () => {
       console.log('connect to broker');
       client.subscribe(topic, (err) => {
@@ -58,10 +65,62 @@ export class DdigService {
           console.log('subscribed to Topic!');
         }
         client.on('message', (message) => {
+          const payload = this.CheckifTempStorage(OwnerID);
+          if (!payload.State) {
+            this.CreateNewTempStorage(payload.Path);
+            this.DataHandeler(message, payload.Path);
+          } else {
+            this.DataHandeler(message, payload.Path);
+          }
           console.log(`Received message on topic ${message.toString()}`);
         });
       });
     });
   }
-  //handelData(clinet: MqttClient){}
+
+  CheckifTempStorage(UserID: number) {
+    const VerfiyDir: string = '../../TempStorageCSV/' + UserID.toString();
+    try {
+      return { Path: VerfiyDir, State: statSync(VerfiyDir).isDirectory() };
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        return { Path: '', State: false };
+      } else console.log(err);
+    }
+  }
+
+  CreateCsvFile(Path: string, fileName?: string, message: string = ''): string {
+    if (fileName !== undefined) {
+      const filePath = Path + '/' + fileName;
+      writeFileSync(filePath, message);
+
+      return filePath;
+    }
+    const currentTime: Date = new Date();
+    const filename: string =
+      currentTime.getFullYear().toString() +
+      currentTime.getDate().toString() +
+      currentTime.getHours().toString() +
+      currentTime.getMinutes().toString();
+    const filePath = Path + '/' + filename;
+    writeFileSync(filePath, message);
+    return filePath;
+  }
+
+  CreateNewTempStorage(UserPath: string) {
+    try {
+      mkdirSync(UserPath);
+      console.log('created Path successfully');
+      const csvMap: string = 'ir_Reading,radReading,beats,TimeStamp\n';
+      writeFileSync(UserPath, undefined, csvMap);
+    } catch (err) {
+      if (err.code === 'EEXIST') {
+        console.log('file already exists');
+      } else {
+        return err;
+      }
+    }
+  }
+
+  DataHandeler(message: string, targetPath: string) {}
 }
