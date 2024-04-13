94667ed1e003b0f8e5faa4d6328917a5402df0b7
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2024-04-13 15:00:52 +0100

    added jwtStrategy to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,14 @@ import { AuthService } from './auth.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
+//import { JwtStrategy } from './startegy/jwt.startegy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './startegy';
+//import { JwtStrategy } from './startegy';
 
 @Module({
-  imports: [JwtModule, ConfigModule],
+  imports: [JwtModule, ConfigModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtModule, JwtService, PrismaService],
+  providers: [AuthService, JwtModule, JwtService, JwtStrategy, PrismaService],
 })
 export class AuthModule {}
