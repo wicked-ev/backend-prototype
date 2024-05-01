6b4245bbe8437f0ac483a70a9d435e732a368fd2
mouaadh sahailia <mouaadhsahailia@gmail.com>
2024-05-01 17:40:18 +0200

    try to fix again

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { ActivateDeviceDto, NoteDto, RNPdto } from './dto/index';
 import { error } from 'console';
 import { Users } from '@prisma/client';
