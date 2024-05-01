78c7708e06a052422067fc754c51931a987e4b85
mouaadh sahailia <99537594+mouaadh11@users.noreply.github.com>
2024-05-01 17:55:19 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { DDIGModule } from './DDIG/DDIG.module';
+import { DDIGModule } from './DDIG/ddig.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
