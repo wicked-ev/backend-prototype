bad751d9e3503e13cbb4898a934f85bb6f12fbc8
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2024-05-10 02:04:19 +0100

    removed ws setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-//import { IoAdapter } from '@nestjs/platform-socket.io';
-import { WsAdapter } from '@nestjs/platform-ws';
-
-import * as http from 'http';
-import * as WebSocket from 'ws';
-//import { WebSocketServer } from '@nestjs/websockets';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,10 +10,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
-  const server = http.createServer(app.getHttpAdapter().getInstance());
-  const wss = new WebSocket.Server({ server });
-  app.useWebSocketAdapter(new WsAdapter(wss));
   await app.listen(5000);
 }
 bootstrap();
